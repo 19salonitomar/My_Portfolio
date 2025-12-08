167b813d735ad2394409a23d4c57f81b908d883f
Saloni Tomar <tomarsaloni19@gmail.com>
2025-12-08 16:52:11 +0530

    Added header Data

---
+++ src/data/headerData.js
@@ -0,0 +1,11 @@
+import resume from '../assets/pdf/resume.pdf'
+import img1 from '../assets/png/img1.png'
+
+
+export const headerData = {
+    name: 'Saloni Tomar',
+    title: "Full Stack Developer & Data Enthusiast",
+    desciption:"Hey 👋, I'm Saloni Tomar, a passionate tech enthusiast and aspiring Full Stack Developer. I believe that discipline turns ambitions into achievements and ideas into reality.",
+    image: img1,
+    resumePdf: resume
+}
