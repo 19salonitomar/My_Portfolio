f9afb4e4b549418f7aba1a9e8c40cc6a1f568830
Saloni Tomar <tomarsaloni19@gmail.com>
2025-12-12 12:55:56 +0530

    Addede Social Links

---
+++ src/data/socialsData.js
@@ -0,0 +1,6 @@
+export const socialsData = {
+    github: 'https://github.com/19salonitomar',
+    linkedIn: 'https://www.linkedin.com/in/saloni-tomar-68821423a/',
+    twitter: 'https://x.com/t03015843',
+    medium: 'https://medium.com/@salonitomar5813'
+}
