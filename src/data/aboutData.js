95fba03b8cfe70ce03b002c6f54ccdc39a5c8b6c
Saloni Tomar <tomarsaloni19@gmail.com>
2025-12-14 11:16:05 +0530

    Added about data

---
+++ src/data/aboutData.js
@@ -0,0 +1,6 @@
+export const aboutData = {
+  title: "Who I Am",
+  description1: "Hello! I'm Saloni Tomar, a driven Full Stack Developer and Data Analyst based in Pune, India. I craft scalable, secure, and high-performance web applications using cutting-edge technologies like React, Angular, Spring Boot, and Node.js.",
+  description2: "With a Master's in Computer Applications and hands-on experience in full-stack development, cloud platforms, and data analytics, I design innovative solutions that solve real-world problems. I excel in Java, Python, and JavaScript, and integrate modern practices like machine learning and agile development to deliver meaningful digital experiences. My mission is to continuously learn, tackle complex challenges, and create impactful software solutions that make a difference.",
+  image: 2
+}
