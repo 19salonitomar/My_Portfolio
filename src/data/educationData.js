d1af989cb115633c27d42c09d572fd0c4e5c744f
Saloni Tomar <tomarsaloni19@gmail.com>
2025-12-16 13:03:44 +0530

    Added Education Data

---
+++ src/data/educationData.js
@@ -0,0 +1,17 @@
+export const educationData = [
+    {
+        id: 1,
+        institution: 'Savitribai Phule Pune University | Pune',
+        course: 'Master of Computer Application (MCA)',
+        startYear: '2021',
+        endYear: '2023'
+    },
+    {
+        id: 2,
+        institution: 'Devi Ahilya Vishwavidyalaya | Indore',
+        course: 'Bachelor of Computer Science (B.Sc.)',
+        startYear: '2018',
+        endYear: '2021'
+    },
+    
+]
