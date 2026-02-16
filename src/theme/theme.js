97b5bf7e1189b8a586c49b879e6d56656a118a02
Saloni Tomar <tomarsaloni19@gmail.com>
2026-02-16 19:17:10 +0530

    Adding theme File

---
+++ src/theme/theme.js
@@ -0,0 +1,115 @@
+/* eslint-disable */
+import { 
+    pinkThemeboy, pinkThemegirl, redThemeboy, redThemegirl,yellowThemeboy,yellowThemegirl,eduPink,eduRed,eduYellow,expPink,expRed,expYellow,contactsPink,contactsRed,contactsYellow
+   
+} from './images'
+
+
+
+export const redThemeLight = {
+    type: 'light',
+    primary: '#f03939',
+    primary400: '#dd4d4d',
+    primary600: '#ea2424',
+    primary80: '#f03939cc',
+    primary50: '#f0393980',
+    primary30: '#f039394d',
+    secondary: '#eaeaea',
+    secondary70: '#eaeaeab3',
+    secondary50: '#eaeaea80',
+    tertiary: '#212121',
+    tertiary80: '#212121cc',
+    tertiary70: '#212121b3',
+    tertiary50: '#21212180',
+    aboutimg1: redThemeboy,
+    aboutimg2: redThemegirl,
+    eduimg: eduRed,
+    expimg: expRed,
+    contactsimg: contactsRed
+}
+export const redThemeDark = {
+    type: 'dark',
+    primary: '#f03939',
+    primary400: '#dd4d4d',
+    primary600: '#ea2424',
+    primary80: '#f03939cc',
+    primary50: '#f0393980',
+    primary30: '#f039394d',
+    secondary: '#212121',
+    secondary70: '#212121b3',
+    secondary50: '#21212180',
+    tertiary: '#eaeaea',
+    tertiary80: '#eaeaeacc',
+    tertiary70: '#eaeaeab3',
+    tertiary50: '#eaeaea80',
+    aboutimg1: redThemeboy,
+    aboutimg2: redThemegirl,
+    eduimg: eduRed,
+    expimg: expRed,
+    contactsimg: contactsRed
+}
+
+export const pinkThemeLight = {
+    type: 'light',
+    primary: '#ff4f93',
+    primary400: '#e56f9d',
+    primary600: '#e14381',
+    primary80: '#ff4f93cc',
+    primary50: '#ff4f9380',
+    primary30: '#ff4f934d',
+    secondary: '#eaeaea',
+    secondary70: '#eaeaeab3',
+    secondary50: '#eaeaea80',
+    tertiary: '#212121',
+    tertiary80: '#212121cc',
+    tertiary70: '#212121b3',
+    tertiary50: '#21212180',
+    aboutimg1: pinkThemeboy,
+    aboutimg2: pinkThemegirl,
+    eduimg: eduPink,
+    expimg: expPink,
+    contactsimg: contactsPink
+}
+export const pinkThemeDark = {
+    type: 'dark',
+    primary: '#ff4f93',
+    primary400: '#e56f9d',
+    primary600: '#e14381',
+    primary80: '#ff4f93cc',
+    primary50: '#ff4f9380',
+    primary30: '#ff4f934d',
+    secondary: '#212121',
+    secondary70: '#212121b3',
+    secondary50: '#21212180',
+    tertiary: '#eaeaea',
+    tertiary80: '#eaeaeacc',
+    tertiary70: '#eaeaeab3',
+    tertiary50: '#eaeaea80',
+    aboutimg1: pinkThemeboy,
+    aboutimg2: pinkThemegirl,
+    eduimg: eduPink,
+    expimg: expPink,
+    contactsimg: contactsPink
+}
+
+export const yellowThemeLight = {
+    type: 'light',
+    primary: '#E9AD35',
+    primary400: '#e8b44c',
+    primary600: '#eeac2a',
+    primary80: '#E9AD35cc',
+    primary50: '#E9AD3580',
+    primary30: '#E9AD354d',
+    secondary: '#eaeaea',
+    secondary70: '#eaeaeab3',
+    secondary50: '#eaeaea80',
+    tertiary: '#212121',
+    tertiary80: '#212121cc',
+    tertiary70: '#212121b3',
+    tertiary50: '#21212180',
+    aboutimg1: yellowThemeboy,
+    aboutimg2: yellowThemegirl,
+    eduimg: eduYellow,
+    expimg: expYellow,
+    contactsimg: contactsYellow
+}
